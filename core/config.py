bd5d51a8ab6d599bbc35eae1947a864c1e12c559
kevin <kevinjivani3010@gmail.com>
2024-06-19 22:12:42 -0400

    Configure MongoDB

--- core/config.py
+++ core/config.py
@@ -1,8 +1,8 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "fastapi_db"
+    DATABASE_URL: str = "mongodb+srv://admin:kevinandjevin@resumelink.ctj0men.mongodb.net/?retryWrites=true&w=majority&appName=ResumeLink"
+    DATABASE_NAME: str = "resume_link_db"
 
     class Config:
         env_file = ".env"
